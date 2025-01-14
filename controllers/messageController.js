abb98eb7de64ebdf1afe5ed325c33f3b57e738a0
cherryy1681 <cherrydev1681@gmail.com>
2025-01-14 13:18:20 +0530

    added getting users latitude and longitude using whatsapp location share

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -1,46 +1,53 @@
-const twilioService = require('../services/twilioService');
-const schedulerService = require('../services/schedulerService');
-const templates = require('../constants/messageTemplates');
+const twilioService = require("../services/twilioService");
+const schedulerService = require("../services/schedulerService");
+const templates = require("../constants/messageTemplates");
 
 const handleWebhook = async (req, res) => {
-    try {
-        const incomingMsg = req?.body?.Body;
-        const senderNumber = req?.body?.From;
+  try {
+    const incomingMsg = req?.body?.Body;
+    const senderNumber = req?.body?.From;
 
-        if (!incomingMsg || !senderNumber) {
-            return res.status(400).json({ 
-                status: 'error', 
-                message: 'Missing required fields' 
-            });
-        }
+    console.log("request: ", req)
 
-        if (incomingMsg.toLowerCase().includes('schedule')) {
-            
-            await twilioService.sendMessage(
-                senderNumber,
-                templates.scheduledConfirmation
-            );
-            await schedulerService.scheduleMessage([senderNumber],
-                'This is a scheduled message that comes 1 minute after you send \'schedule\' message'
-            );
-        } else {
-            await twilioService.handleIncomingMessage(incomingMsg, senderNumber);
-        }
-
-        return res.status(200).json({ 
-            status: 'success', 
-            message: 'Response sent' 
-        });
-    } catch (error) {
-        console.error('Error in handleWebhook:', error);
-        return res.status(500).json({ 
-            status: 'error', 
-            message: 'Error processing message',
-            error: error.message 
-        });
+    if (!senderNumber) {
+      return res.status(400).json({
+        status: "error",
+        message: "Missing required fields",
+      });
     }
+    if (req.body.Latitude && req.body.Longitude) {
+      return twilioService.sendMessage(
+        senderNumber,
+        `Your latitude is : ${req.body.Latitude} and longitude is: ${req.body.Longitude}`
+      );
+    }
+    if (incomingMsg.toLowerCase().includes("schedule")) {
+      await twilioService.sendMessage(
+        senderNumber,
+        templates.scheduledConfirmation
+      );
+      await schedulerService.scheduleMessage(
+        [senderNumber],
+        "This is a scheduled message that comes 1 minute after you send 'schedule' message"
+      );
+    } else {
+      await twilioService.handleIncomingMessage(incomingMsg, senderNumber);
+    }
+
+    return res.status(200).json({
+      status: "success",
+      message: "Response sent",
+    });
+  } catch (error) {
+    console.error("Error in handleWebhook:", error);
+    return res.status(500).json({
+      status: "error",
+      message: "Error processing message",
+      error: error.message,
+    });
+  }
 };
 
 module.exports = {
-    handleWebhook,
+  handleWebhook,
 };
